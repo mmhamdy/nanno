d2ce450f51e78904553b5399e8b54c5f85e395c6
mmhamdy <mohamdy2772@gmail.com>
2023-08-07 15:47:29 +0300

    add examples to train and evaluate

--- nanno/core.py
+++ nanno/core.py
@@ -1,11 +1,24 @@
 import numpy as np
 
 class ANN:
-    def __init__(self, layers=None):
+    def __init__(self, layers=None, expected_range=(-1, 1)):
         self.layers = layers
+        self.n_iter_train = int(100)
+        self.n_iter_evaluate = int(100)
+        self.expected_range = expected_range
 
     def train(self, training_set):
-        pass
+        for iteration in range(self.n_iter_train):
+            x = self.normalize(next(training_set()).ravel())
+            print(x)
 
     def evaluate(self, evaluation_set):
-        pass
+        for iteration in range(self.n_iter_evaluate):
+            x = self.normalize(next(evaluation_set()).ravel())
+
+    def normalize(self, values):
+        min_val = self.expected_range[0]
+        max_val = self.expected_range[1]
+        scale_factor = max_val - min_val
+        offset_factor = min_val
+        return (values - offset_factor) / scale_factor - 0.5
--- run.py
+++ run.py
@@ -4,10 +4,11 @@ from nanno.core import ANN
 training_set, evaluation_set = dl.get_two_by_two()
 
 sample = next(training_set())
+input_value_range = (0, 1)
 n_pixels = sample.shape[0] * sample.shape[1]
 n_nodes = [n_pixels, n_pixels]
 
-autoencoder = ANN(layers=None)
+autoencoder = ANN(layers=None, expected_range=input_value_range)
 
 if __name__ == "__main__": 
     autoencoder.train(training_set)
