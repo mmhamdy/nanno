ad293652bd6a288235dd3683f4f6cc791d338f72
mmhamdy <mohamdy2772@gmail.com>
2023-08-18 07:11:56 +0300

    rewrite core

--- nanno/core.py
+++ nanno/core.py
@@ -1,24 +1,23 @@
 import numpy as np
 
-class ANN:
-    def __init__(self, layers=None, expected_range=(-1, 1)):
-        self.layers = layers
-        self.n_iter_train = int(100)
-        self.n_iter_evaluate = int(100)
-        self.expected_range = expected_range
+class Operation:
+    """
+    Base class for an operation
+    """
+    def __init__(self, params=None):
+        if params:
+            self.param = params
 
-    def train(self, training_set):
-        for iteration in range(self.n_iter_train):
-            x = self.normalize(next(training_set()).ravel())
-            print(x)
+    def forward():
+        raise NotImplementedError
 
-    def evaluate(self, evaluation_set):
-        for iteration in range(self.n_iter_evaluate):
-            x = self.normalize(next(evaluation_set()).ravel())
+    def backward():
+        raise NotImplementedError
 
-    def normalize(self, values):
-        min_val = self.expected_range[0]
-        max_val = self.expected_range[1]
-        scale_factor = max_val - min_val
-        offset_factor = min_val
-        return (values - offset_factor) / scale_factor - 0.5
+
+class Layer:
+    """
+    Base class for a layer
+    """
+    def __init__(self):
+        pass
